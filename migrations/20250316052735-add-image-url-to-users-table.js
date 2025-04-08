5f7966da8cd4fd026bfad0c70c7a90c03a8a0a95
HemanthKumar-CN <hemanth.html+github@gmail.com>
2025-04-08 09:57:47 +0530

    updating image_url creation migration file

--- migrations/20250316052735-add-image-url-to-users-table.js
+++ migrations/20250316052735-add-image-url-to-users-table.js
@@ -3,10 +3,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("users", "image_url", {
-      type: Sequelize.STRING,
-      allowNull: true, // Allow null initially
-    });
+    const table = await queryInterface.describeTable("users");
+
+    if (!table.image_url) {
+      await queryInterface.addColumn("users", "image_url", {
+        type: Sequelize.STRING,
+        allowNull: true, // Allow null initially
+      });
+    }
   },
 
   async down(queryInterface, Sequelize) {
