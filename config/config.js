980240adaa3b1bc5e10c868e8c9b61b8ae0791d3
HemanthKumar-CN <hemanth.html+github@gmail.com>
2025-04-08 10:11:18 +0530

    updating config.js for development env

--- config/config.js
+++ config/config.js
@@ -2,10 +2,11 @@ require("dotenv").config(); // just in case
 
 module.exports = {
   development: {
-    username: "postgres",
-    password: "Hem@81055",
-    database: "jobtrack",
-    host: "127.0.0.1",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
     dialect: "postgres",
   },
   test: {
