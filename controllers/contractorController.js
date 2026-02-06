b47fefd4baa7353d1895c7895c4e7b108b97ac17
HemanthKumar-CN <hemanth.html+github@gmail.com>
2026-02-06 21:29:37 +0530

    Fix: updating the contractor create logic

--- controllers/contractorController.js
+++ controllers/contractorController.js
@@ -105,7 +105,7 @@ const createContractor = async (req, res) => {
     console.log(req.body);
 
     // Check for required fields
-    if (!first_name || !last_name || !company_name || !city || !phone) {
+    if (!first_name || !last_name || !company_name || !city) {
       return res
         .status(400)
         .json({ error: "All required fields must be filled" });
